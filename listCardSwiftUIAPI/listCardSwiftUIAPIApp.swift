dd0ffdd8db8c8ddbae1d1e150b0be1bbfc63f7cd
Tai Nguyen <nguyenductai.job@gmail.com>
2022-06-18 16:50:20 +0700

    Init

---
+++ listCardSwiftUIAPI/listCardSwiftUIAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  listCardSwiftUIAPIApp.swift
+//  listCardSwiftUIAPI
+//
+//  Created by Tai Nguyen on 18/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct listCardSwiftUIAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
